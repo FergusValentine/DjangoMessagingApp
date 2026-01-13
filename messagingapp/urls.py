f942c09687ec70d09c7ab3b3c8b69251e9d60dee
Fergus Valentine <fergushenryvalentine@icloud.com>
2026-01-13 15:00:33 +0000

    Initial commit

--- messagingapp/urls.py
+++ messagingapp/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_page, name='register'),
+    path('profile/<str:pk>/', views.user_profile, name='user_profile'),
+
+    path('', views.home_page, name='home'),
+
+    path('room/<str:pk>/', views.room_page, name='room'),
+    path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
 ]
--- messagingapp/views.py
+++ messagingapp/views.py
@@ -1,5 +1,155 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
 
-def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'messagingapp/home.html')
+from .models import Room, Topic, Message
+from .forms import RoomForm
+
+def login_page(request: HttpRequest) -> HttpResponse:
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Incorrect username or password')
+
+    context = {'page': page}
+    return render(request, 'messagingapp/login_register.html', context)
+
+def logout_user(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect('home')
+
+def register_page(request: HttpRequest) -> HttpResponse:
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occurred during registration")
+
+    context = {'form': form}
+    return render(request, 'messagingapp/login_register.html', context)
+
+def home_page(request: HttpRequest) -> HttpResponse:
+    q = request.GET.get('q') if request.GET.get('q') else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
+    return render(request, 'messagingapp/home.html', context)
+
+def room_page(request: HttpRequest, pk: str) -> HttpResponse:
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body'),
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
+    return render(request, 'messagingapp/room.html', context)
+
+def user_profile(request: HttpRequest, pk: str) -> HttpResponse:
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
+    context = {'user': user, 'rooms':rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'messagingapp/profile.html', context)
+
+@login_required(login_url='login')
+def create_room(request: HttpRequest):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'messagingapp/room_form.html', context)
+
+@login_required(login_url='login')
+def update_room(request: HttpRequest, pk: str) -> HttpResponse:
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('Invalid permission')
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'messagingapp/room_form.html', context)
+
+@login_required(login_url='login')
+def delete_room(request: HttpRequest, pk: str) -> HttpResponse:
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('Invalid permission')
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'messagingapp/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def delete_message(request: HttpRequest, pk: str) -> HttpResponse:
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Invalid permission')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'messagingapp/delete.html', {'obj': message})
