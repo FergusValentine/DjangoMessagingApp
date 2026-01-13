19e07dfa5a27078a77a099cfa64c2f6d2ee6856a
Fergus Valentine <fergushenryvalentine@icloud.com>
2026-01-13 15:00:33 +0000

    Initial commit

---
+++ messagingapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'messagingapp/home.html')
